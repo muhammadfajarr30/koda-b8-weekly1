61a4781114111a9116611fe1d1e3e608cefe36e4
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-21 18:03:25 +0700

    refactor: move category logic to features/category.js

---
+++ features/category.js
@@ -0,0 +1,40 @@
+import { ask } from "../utils/input.js";
+import menu from "../menu.json" with { type: "json" };
+import { showItems } from "./showItems.js";
+import { mainMenu } from "../index.js";
+
+const foods = [];
+const drinks = [];
+const sideDish = [];
+
+foods.push(...menu.foods);
+drinks.push(...menu.drinks);
+sideDish.push(...menu.sideDish);
+
+async function catergoryMenu() {
+  console.log("\n=== SELECT CATEGORY ===");
+  console.log("1. NOODLES");
+  console.log("2. BEVERAGES");
+  console.log("3. SIDE DISH");
+  console.log("4. Back");
+
+  const ans = await ask("Select Menu: ");
+  switch (ans) {
+    case "1":
+      showItems(foods, "NOODLES");
+      break;
+    case "2":
+      showItems(drinks, "BEVERAGES");
+      break;
+    case "3":
+      showItems(sideDish, "DIMSUM");
+      break;
+    case "4":
+      mainMenu();
+      break;
+    default:
+      console.log("Invalid Category Option");
+      catergoryMenu();
+  }
+}
+export { catergoryMenu };
