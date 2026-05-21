86a1334a831c598e8c7131e1dce6733c92377b8a
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-21 23:26:06 +0700

    refactor: move function showCart into ./features/cart.js

---
+++ features/cart.js
@@ -0,0 +1,22 @@
+import { mainMenu } from "../index.js";
+import { cart } from "../index.js";
+
+function showCart() {
+  console.log("\n=== CART ===");
+  if (cart.length == 0) {
+    console.log("Cart can't be empty!");
+  } else {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      console.log(
+        `${i + 1}. ${cart[i].name} x${cart[i].amount} Rp.${cart[i].subtotal}`,
+      );
+      total += cart[i].subtotal;
+      console.log(`Total Order: ${total}`);
+    }
+  }
+
+  mainMenu();
+}
+
+export { showCart };
