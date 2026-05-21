47cf966365f41769a2236292b1d891cd854382fe
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-21 23:23:41 +0700

    refactor:move function checkout into features/checkout.js

---
+++ features/checkout.js
@@ -0,0 +1,41 @@
+import { ask } from "../utils/input.js";
+import { cart } from "../index.js";
+import { mainMenu } from "../index.js";
+import { catergoryMenu } from "./category.js";
+import { payment } from "./payment.js";
+
+export let total = 0;
+async function checkout() {
+  console.log(`\n======== CHECKOUT ========`);
+
+  if (cart.length === 0) {
+    console.log("you have not order yet!");
+    return mainMenu();
+  }
+
+  for (let i = 0; i < cart.length; i++) {
+    console.log(
+      `${i + 1}. ${cart[i].name} x${cart[i].amount} Rp.${cart[i].subtotal}`,
+    );
+    total += cart[i].subtotal;
+  }
+
+  console.log(`Total Price: Rp.${total}`);
+  console.log("===========================");
+  const askOrderedItem = await ask(
+    "Let us know if these are the correct items(y/n):  ",
+  );
+  switch (askOrderedItem) {
+    case "y":
+      payment();
+      break;
+    case "n":
+      catergoryMenu();
+      break;
+    default:
+      console.log("Invalid Answer!");
+      return checkout();
+  }
+}
+
+export { checkout };
