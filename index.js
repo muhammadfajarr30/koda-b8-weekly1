e0aeeb9f130ed93f4f3eb2c5e5065e3c42c756eb
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-21 15:17:46 +0700

    refactor: use async await function in index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import { createInterface } from "node:readline";
 import menu from "./menu.json" with { type: "json" };
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+import { ask, close } from "./utils/input.js";
 
 const foods = [];
 const drinks = [];
@@ -17,64 +13,62 @@ let cart = [];
 
 mainMenu();
 
-function mainMenu() {
+async function mainMenu() {
   console.log("\n=== MIE GACOAN ===");
   console.log("1. Category Menu");
   console.log("2. Cart");
   console.log("3. Checkout");
   console.log("4. Exit");
 
-  rl.question("Select Option: ", function (ans) {
-    switch (ans) {
-      case "1":
-        catergoryMenu();
-        break;
-      case "2":
-        showCart();
-        break;
-      case "3":
-        checkout();
-        break;
-      case "4":
-        console.log("Thank You, See You Again!");
-        rl.close();
-        break;
-      default:
-        console.log("Invalid Option!");
-        mainMenu();
-    }
-  });
+  const ans = await ask("select Option: ");
+  switch (ans) {
+    case "1":
+      catergoryMenu();
+      break;
+    case "2":
+      showCart();
+      break;
+    case "3":
+      checkout();
+      break;
+    case "4":
+      console.log("Thank You, See You Again!");
+      close();
+      break;
+    default:
+      console.log("Invalid Option!");
+      mainMenu();
+  }
 }
 
-function catergoryMenu() {
+async function catergoryMenu() {
   console.log("\n=== SELECT CATEGORY ===");
   console.log("1. NOODLES");
   console.log("2. BEVERAGES");
   console.log("3. SIDE DISH");
   console.log("4. Back");
 
-  rl.question("Select Category: ", function (ans) {
-    switch (ans) {
-      case "1":
-        showItems(foods, "NOODLES");
-        break;
-      case "2":
-        showItems(drinks, "BEVERAGES");
-        break;
-      case "3":
-        showItems(sideDish, "DIMSUM");
-        break;
-      case "4":
-        mainMenu();
-        break;
-      default:
-        console.log("Invalid Category Option");
-        catergoryMenu();
-    }
-  });
+  const ans = await ask("Select Menu: ");
+  switch (ans) {
+    case "1":
+      showItems(foods, "NOODLES");
+      break;
+    case "2":
+      showItems(drinks, "BEVERAGES");
+      break;
+    case "3":
+      showItems(sideDish, "DIMSUM");
+      break;
+    case "4":
+      mainMenu();
+      break;
+    default:
+      console.log("Invalid Category Option");
+      catergoryMenu();
+  }
 }
 
-function showItems(menu, categoryName) {
+async function showItems(menu, categoryName) {
   console.log(`\n=== ${categoryName} ===`);
 
   for (let i = 0; i < menu.length; i++) {
@@ -83,38 +77,36 @@ function showItems(menu, categoryName) {
 
   console.log(`${menu.length + 1}. Back`);
 
-  rl.question("Select Menu: ", function (ans) {
-    let choice = Number(ans);
-    switch (choice) {
-      case menu.length + 1:
-        catergoryMenu();
-        break;
-      default:
-        if (choice >= 1 && choice <= menu.length) {
-          const selectedItem = menu[choice - 1];
+  const input = await ask("Select Menu: ");
+  const choice = Number(input);
+  switch (choice) {
+    case menu.length + 1:
+      catergoryMenu();
+      break;
+    default:
+      if (choice >= 1 && choice <= menu.length) {
+        const selectedItem = menu[choice - 1];
 
-          rl.question("Amount: ", function (ans) {
-            let amount = Number(ans);
-            if (isNaN(amount) || amount < 1) {
-              console.log("Input Amount and should be a number");
-              return showItems(menu, categoryName);
-            }
-
-            cart.push({
-              name: selectedItem.name,
-              price: selectedItem.price,
-              amount: amount,
-              subtotal: amount * selectedItem.price,
-            });
-            console.log(`${selectedItem.name} x${amount} added successfully`);
-            catergoryMenu();
-          });
-        } else {
-          console.log("Invalid Items Option");
-          showItems(menu, categoryName);
+        const inputAmount = await ask("Amount :");
+        const amount = Number(inputAmount);
+        if (isNaN(amount) || amount < 1) {
+          console.log("Input Amount and should be a number");
+          return showItems(menu, categoryName);
         }
-    }
-  });
+
+        cart.push({
+          name: selectedItem.name,
+          price: selectedItem.price,
+          amount: amount,
+          subtotal: amount * selectedItem.price,
+        });
+        console.log(`${selectedItem.name} x${amount} added successfully`);
+        catergoryMenu();
+      } else {
+        console.log("Invalid Items Option");
+        showItems(menu, categoryName);
+      }
+  }
 }
 
 function showCart() {
@@ -134,7 +126,7 @@ function showCart() {
   mainMenu();
 }
 
-function checkout() {
+async function checkout() {
   console.log(`\n=== CHECKOUT ===`);
 
   if (cart.length === 0) {
@@ -152,19 +144,18 @@ function checkout() {
 
   console.log(`Total Price: Rp.${total}`);
 
-  rl.question("Input money: ", function (ans) {
-    let payment = Number(ans);
-    if (isNaN(payment) || payment <= 0) {
-      console.log("Payment must be a number");
-      return checkout();
-    }
-    if (payment < total) {
-      console.log(`Insufficient money! less Rp. ${total - payment} `);
-      return checkout();
-    }
-    let change = payment - total;
-    console.log(`Change: ${change}`);
-    console.log("Thank You For Your Order :) !");
-    rl.close();
-  });
+  const inputMoney = await ask("Input Money : ");
+  const payment = Number(inputMoney);
+  if (isNaN(payment) || payment <= 0) {
+    console.log("Payment must be a number");
+    return checkout();
+  }
+  if (payment < total) {
+    console.log(`Insufficient money! less Rp. ${total - payment} `);
+    return checkout();
+  }
+  let change = payment - total;
+  console.log(`Change: ${change}`);
+  console.log("Thank You For Your Order :) !");
+  close();
 }
