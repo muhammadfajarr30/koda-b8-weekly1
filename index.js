cbc25e8be897a5715495c7a0cb11d8e44ff1a63c
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-27 13:02:26 +0700

    refactor: add function handleCategory for handling menu and for better navigation and flow

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-import { ask, close } from "./utils/input.js";
-import { categoryMenu } from "./features/category.js";
+import { ask, askNumeral, close } from "./utils/input.js";
+import { categoryMenu, showItems } from "./features/category.js";
 import { showCart } from "./features/cart.js";
 import { checkout } from "./features/checkout.js";
+import menu from "./menu.json" with { type: "json" };
 
 mainMenu();
 
 async function mainMenu() {
-  try {
-    console.log("\n=== WELCOME TO MIE GACOAN ===");
-    console.log("Please Select Your Option");
-    console.log("1. Category Menu");
-    console.log("2. Cart");
-    console.log("3. Checkout");
-    console.log("4. Exit");
-    console.log("============================");
+  while (true) {
+    try {
+      console.log("\n=== WELCOME TO MIE GACOAN ===");
+      console.log("1. Category");
+      console.log("2. Cart");
+      console.log("3. Checkout");
+      console.log("4. Exit");
+      console.log("===========================");
 
-    const ans = await ask("select Option: ");
-    switch (ans) {
-      case "1":
-        await categoryMenu();
-        break;
-      case "2":
-        await showCart();
-        break;
-      case "3":
-        await checkout();
-        break;
-      case "4":
-        console.log("Thank You, See You Again!");
-        close();
-        return;
-        break;
-      default:
-        console.log("Invalid Option!");
-        return mainMenu();
+      const ans = await askNumeral("Select Menu: ");
+
+      switch (ans) {
+        case "1":
+          await handleCategory();
+          break;
+
+        case "2":
+          showCart();
+          break;
+
+        case "3":
+          await checkout();
+          break;
+
+        case "4":
+          console.log("Thank you, see you again!");
+          close();
+          return;
+
+        default:
+          console.log("Invalid Option");
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-    return mainMenu();
+  }
+}
+
+async function handleCategory() {
+  while (true) {
+    const result = await categoryMenu();
+
+    switch (result) {
+      case "NOODLES":
+        await showItems(menu.foods, "NOODLES");
+        break;
+
+      case "BEVERAGES":
+        await showItems(menu.drinks, "BEVERAGES");
+        break;
+
+      case "SIDE_DISH":
+        await showItems(menu.sideDish, "DIMSUM");
+        break;
+
+      case "BACK":
+        return;
+    }
   }
 }
 
